76652a5aa008f1fd6c7b0b1568e19134955f0d12
Younes Elhamss <youneselhamss@gmail.com>
2022-05-31 07:22:56 +0200

    unit test

--- src/test/java/com/example/programmeringseksamen/controllers/CykelRytterControllerTest.java
+++ src/test/java/com/example/programmeringseksamen/controllers/CykelRytterControllerTest.java
@@ -1,46 +1,31 @@
 package com.example.programmeringseksamen.controllers;
 
 import com.example.programmeringseksamen.domain.models.CykelRytter;
+import com.example.programmeringseksamen.domain.models.Hold;
 import com.example.programmeringseksamen.repositories.CykelRytterRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class ControllerTest {
+class CykelRytterControllerTest {
 
-    /*
     @Autowired
-    private ControllerRepository controllerRepository;
+    private CykelRytterRepository cykelRytterRepository;
 
     @Test
     void create() {
+        Hold hold = new Hold();
+        CykelRytter cykelRytter = new CykelRytter(1L,"Younes", 1L, hold); //1L betyder Long
+        cykelRytterRepository.save(cykelRytter);
 
-        CykelRytter cykelRytter = new CykelRytter(1L,"Younes"); //1L betyder Long
-        controllerRepository.save(cykelRytter);
-
-        assertEquals(cykelRytter.getName(), controllerRepository.findById(1L).get().getName());
+        assertEquals(cykelRytter.getName(), cykelRytterRepository.findById(1L).get().getName());
     }
 
 
-
-    @Test
-    void read() {
-    }
-
-    @Test
-    void readById() {
-    }
-
-    @Test
-    void update() {
-    }
-
-    @Test
-    void delete() {
-    }
-
-     */
 }
